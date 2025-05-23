f29aa51446c4869c221f4415b14f7a2303639b1c
Artiom Cozlovici <kozakovi4@outlook.com>
2025-05-23 22:54:05 +0300

    style: Improve debug output for WiFi credentials handling in setWifiCredentials method

--- src/Configuration.cpp
+++ src/Configuration.cpp
@@ -55,12 +55,30 @@ void Configuration::setWifiCredentials(const char *ssid, const char *password)
 {
     strncpy(this->wifiCredentials.ssid, ssid, sizeof(this->wifiCredentials.ssid) - 1);
     strncpy(this->wifiCredentials.password, password, sizeof(this->wifiCredentials.password) - 1);
-    
+#ifdef DEBUG
+    Serial.print("[Configuration] Checking WiFi credentials magic value ... ");
+#endif
     if (this->wifiCredentialsMagicValue != WIFI_CREDENTIALS_MAGIC_VALUE)
     {
+#ifdef DEBUG
+        Serial.println("Missing.");
+        Serial.print("[Configuration] Writing WiFi credentials magic value: ");
+        Serial.println(WIFI_CREDENTIALS_MAGIC_VALUE);
+#endif
         this->wifiCredentialsMagicValue = WIFI_CREDENTIALS_MAGIC_VALUE;
         EEPROM.put(WIFI_CREDENTIALS_MAGIC_OFFSET, this->wifiCredentialsMagicValue);
     }
+#ifdef DEBUG
+    else
+    {
+        Serial.println("Found.");
+    }
+    Serial.println("[Configuration] Writing WiFi credentials to EEPROM:");
+    Serial.print("  SSID: ");
+    Serial.println(this->wifiCredentials.ssid);
+    Serial.print("  Password: ");
+    Serial.println(this->wifiCredentials.password);
+#endif
     EEPROM.put(WIFI_CREDENTIALS_OFFSET, this->wifiCredentials);
 }
 
