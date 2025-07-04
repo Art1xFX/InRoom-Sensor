5697a4c164e2a390f2d2ea74b9f6908099583572
Artiom Cozlovici <kozakovi4@outlook.com>
2025-07-04 13:31:37 +0300

    feat: Add MQTT user and password fields to configuration

--- include/json/Configuration.h
+++ include/json/Configuration.h
@@ -10,6 +10,8 @@ namespace json
         char wifi_password[64];
         char mqtt_host[255];
         uint16_t mqtt_port;
+        char mqtt_user[255];
+        char mqtt_password[255];
         char mqtt_data_topic[255];
 
         Configuration() = default;
